bea35c5edeb1396594b062cc1f5978d1c85f5e98
Yong Hu <yhu@bnl.gov>
2013-11-13 15:28:10 -0500

    use try ... except to avoid Subscription closed issue; added systemUpdateRate

--- gaussFit.py
+++ gaussFit.py
@@ -3,20 +3,20 @@
 from pkg_resources import require
 require('cothread')
 
-import sys
+import sys, os
 #import numpy as np
 #import scipy as sp
-import cothread
-
-from cothread.catools import *
-#from cothread.catools import caget, caput, camonitor, FORMAT_RAW
+os.environ["EPICS_CA_MAX_ARRAY_BYTES"] = '40000000'
+from cothread import WaitForQuit
+from cothread.catools import caget, caput, camonitor, FORMAT_TIME
 from scipy.optimize import leastsq
 from numpy import exp, arange, sqrt, asarray
+
+tsBuffer = [0]*2
 
 if len(sys.argv) != 2:
     print('must use camera prefix such as LTB-BI{VF:1BD1} for argv[1]')
     exit()
-
 cam  = sys.argv[1]
 
 def peval(p, x):
@@ -53,10 +53,27 @@ def gaussfit(p0, data):
     return bp
 
 def callback(value):
+    tsBuffer.append(value.timestamp)
+    #print(tsBuffer)
+    if (tsBuffer[-1] - tsBuffer[-2])>0.02:
+        updateRate = 1.0/(tsBuffer[-1] - tsBuffer[-2])
+        #print("system update rate: %.3f"%updateRate)
+        try:
+            caput('%sSysUpdateRate-I'%(cam), updateRate)
+        except:
+            print('can not caput SysUpdateRate')
+            return
+    tsBuffer.pop(0)
+    #print(tsBuffer)
+    
     recname=value.name
     #dire (direction/axis) is X or Y
     dire=recname[-5]
-    size=caget('%sROI1:Size%s_RBV'%(cam,dire))
+    try:
+        size=caget('%sROI1:Size%s_RBV'%(cam,dire))
+    except:
+        print("can't get size of ROI")
+        return
     #data=+value[1:size-1]
     #data: array/waveform data; image profile/intensity
     wf = +value[0:size]
@@ -67,20 +84,24 @@ def callback(value):
     #fittederr = sum((fittedwf-wf)**2)
     fittederr = sum(((fittedwf-wf)/bestp[0])**2)/size
     #print('%s fitted error: %f'%(dire, fittederr))
-    caput("%s%s-Gauss:Max-I"%(cam,dire), bestp[0])
-    caput("%s%s-Gauss:Mean-I"%(cam, dire), bestp[1])
-    caput("%s%s-Gauss:Sigma-I"%(cam, dire), abs(bestp[2]))
-    caput("%s%s-Gauss:Offset-I"%(cam, dire), bestp[3])
-    caput("%s%s-Gauss:FittedErr-I"%(cam, dire), fittederr)
-    caput("%sStats1:Peak%s_RBV"%(cam, dire), initp[4])
-    #print('initial %s peak index: %d'%(dire,initp[4]))
-    #print('initial/fitted %s offsets: %d / %d'%(dire,initp[3],bestp[3]))
-    caput("%s%s-Gauss:Data-I"%(cam,dire), fittedwf)
+    try:
+        caput("%s%s-Gauss:Max-I"%(cam,dire), bestp[0])
+        caput("%s%s-Gauss:Mean-I"%(cam, dire), bestp[1])
+        caput("%s%s-Gauss:Sigma-I"%(cam, dire), abs(bestp[2]))
+        caput("%s%s-Gauss:Offset-I"%(cam, dire), bestp[3])
+        caput("%s%s-Gauss:FittedErr-I"%(cam, dire), fittederr)
+        caput("%sStats1:Peak%s_RBV"%(cam, dire), initp[4])
+        #print('initial %s peak index: %d'%(dire,initp[4]))
+        #print('initial/fitted %s offsets: %d / %d'%(dire,initp[3],bestp[3]))
+        caput("%s%s-Gauss:Data-I"%(cam,dire), fittedwf)
+    except:
+        print("can't caput the results out")
+        return
 
 def main():
-    camonitor("%sStats1:ProfileAverageX_RBV"%(cam), callback, format=FORMAT_RAW)
-    camonitor("%sStats1:ProfileAverageY_RBV"%(cam), callback, format=FORMAT_RAW)
-    cothread.WaitForQuit()
+    camonitor("%sStats1:ProfileAverageX_RBV"%(cam), callback, format=FORMAT_TIME)
+    camonitor("%sStats1:ProfileAverageY_RBV"%(cam), callback, format=FORMAT_TIME)
+    WaitForQuit()
 
 if __name__ == "__main__":
     main()
